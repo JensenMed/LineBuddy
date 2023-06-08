11afa43cf3a76e2510efc1b14bcb5baa168f7efe
Jensen Medeiros <jensenmed2@gmail.com>
2023-06-08 19:29:22 -0400

    Removing android/ios + node

--- src/Component.tsx
+++ src/Component.tsx
@@ -1,6 +1,9 @@
 
 
 import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './screens/Home';
 import Welcome from './screens/Welcome';
 import SignUp from './screens/SignUp';
@@ -9,10 +12,22 @@ import Admin from './screens/Admin';
 import Settings from './screens/Settings';
 import Admin_Settings from './screens/Admin_Settings';
 
+// const Test = require('react-navigation__native');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// const Stack = createNativeStackNavigator();
 
 const Component = () => {
   return (
     <View>
+      {/* <NavigationContainer> */}
+      {/* <Stack.Navigator>
+        <Stack.Screen
+          name="Settings"
+          component={Settings}
+          // options={{title: 'Welcome'}}
+        />
+      </Stack.Navigator> */}
+    {/* </NavigationContainer> */}
       {/* <Home/> */}
       {/* <SignUp/> */}
       {/* <Search/> */}
