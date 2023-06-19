8f284f79517e2c07443b61062044323e42cf5cd1
Jensen Medeiros <jensenmed2@gmail.com>
2023-06-19 19:50:00 -0400

    Fixed map issue

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -22,16 +22,16 @@ import Triangle from 'react-native-triangle';
 
 
 const Search = () => {
-  // const latitude = 37.773972;
-  // const longitude = -122.431297;
+  const latitude = 37.773972;
+  const longitude = -122.431297;
   let radius = 50000;
 
 
   // const url =
-  // "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=" + radius + "&key=" +
+  // "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=" + radius + "&key=" + "0392bce78e57034b952f3a6794f83f78e5b0b38a9feabafa226bebd72f275fb7"
 
-
-
+  // const url = "https://serpapi.com/search.json?engine=google_maps" + "&q=pizza" + "&ll=@" + latitude + ","+ "-" + longitude + ",15.1z" + "&type=search" + "&api_key=" + "0392bce78e57034b952f3a6794f83f78e5b0b38a9feabafa226bebd72f275fb7"
+  const url = 'https://serpapi.com/search.json?engine=google_maps&q=pizza&ll=@42.9849,-81.2453,15.1z&type=search&api_key=0392bce78e57034b952f3a6794f83f78e5b0b38a9feabafa226bebd72f275fb7'
 
 
   //Selected places marker
@@ -134,22 +134,23 @@ const Search = () => {
   };
   const getPlacesData = async (url: string, i: number, idk: any[]) => {
     if (i >= 119) {
-      // console.log(nearbyPlacesNames)
       return i;
     } else {
       try {
         let a = 0
         let res = await axios.get(url)
-        let nextPageUrl =
-          res.data.serpapi_pagination.next +
+        let nextPageUrl = res.data.serpapi_pagination.next + '&api_key=0392bce78e57034b952f3a6794f83f78e5b0b38a9feabafa226bebd72f275fb7'
+
         // Checks if there is a next page token
         if (nextPageUrl !== undefined) {
+          // console.log("djkskdjd")
           while (a < res.data.local_results.length){
             // Add the results to the NeabyPlacesObj and nearbyPlacesNames array
             if ( res.data.local_results[a].title + " " + res.data.local_results[a].address != undefined ) {
               nearbyPlacesObjs.push(res.data.local_results[a]);
               nearbyPlacesNames.push(res.data.local_results[a].title + " " + res.data.local_results[a].address);
               let newPlace = {name:res.data.local_results[a].title + " " + res.data.local_results[a].address,selected:false}
+              // console.log(nearbyPlacesNames)
 
               // setSelectedPlacesObj({...selectedPlacesObj2, [newPlace.id]: newPlace})
               // setSelectedPlacesObj([...selectedPlacesObj2, newPlace]);
@@ -189,6 +190,8 @@ const Search = () => {
       let idk = []
       let i = 0;
       let test = await getPlacesData(url, i, idk)
+
+
       // let idk2 = []
       // for(let j = 0; j < nearbyPlacesNames.length; j++){
       //   // let newPlace = [nearbyPlacesNames[j],j]
@@ -295,6 +298,7 @@ const Search = () => {
         testVal = obj.selected
       }
     });
+    // console.log(testVal)
     //  // Adds values to markers on map
     //  selectedPlaces.map((place) => {
     //   let inSelcted = nearbyPlacesNames.indexOf(place)
@@ -324,6 +328,7 @@ const Search = () => {
     // console.log(markers)
     // Will add value to selectedPlaces array if testVal is true (value is selected) and it is not already included in the array
     if (testVal && selectedPlaces.includes(item) === false) {
+      // console.log('kllll')
        // Adds values to markers on map
       //  setS
     //  selectedPlaces.map((place) => {
@@ -351,66 +356,36 @@ const Search = () => {
     //   // setMarkers([...markers, {key: markers.length, title: placeSelected.title , latitude: placeSelected.gps_coordinates.latitude, longitude: placeSelected.gps_coordinates.longitude, description: placeSelected.type}])
     // })
     // Adds value to selecetedPlaces Array
-      setSelectedPlaces([...selectedPlaces, item]);
-
+      selectedPlaces.push(item)
+      setSelectedPlaces(selectedPlaces);
+  
+      // console.log(selectedPlaces)
       // add value to markers
 
       selectedPlaces.map((place) => {
+        // console.log(place)
         let inSelcted = nearbyPlacesNames.indexOf(place)
         objLocation = inSelcted
         let placeSelected = nearbyPlacesObjs[inSelcted] // grabs value from nearbyPlacesObj via index
         let itemFound = false
+        // console.log("jjjjj")
         for( let i = 0; i < markers.length; i++ ){
           if(markers[i].title + " " + placeSelected.address === item){
             itemFound = true;
           }
         }
-        // console.log(itemFound)
         if(itemFound == false){
           let randomKey = Math.floor(Math.random() * 10000) + 1;
-          // console.log(placeSelected.title)
           setMarkers([...markers, {key: randomKey, title: placeSelected.title , latitude: placeSelected.gps_coordinates.latitude, longitude: placeSelected.gps_coordinates.longitude, description: placeSelected.type, address: placeSelected.address}])
         }
-
       })
-      // console.log(markers)
+
     } else if (testVal && selectedPlaces.includes(item) === true){
       // dont do anything just acknowledge that its already in the array
 
     } else {
-      // If the vale of testVal is false or its already in the array then remove it
-      // if(markers.includes(nearbyPlacesObjs[objLocation].title)){
-      //   console.log("KDlkdls")
-      // }
-      // if(!markers.includes(nearbyPlacesObjs[objLocation].title)){
-      //   console.log("dksj")
-      // }
-      // selectedPlaces.map((place) => {
-      //   let inSelcted = nearbyPlacesNames.indexOf(place)
-      //   objLocation = inSelcted
-      //   let placeSelected = nearbyPlacesObjs[inSelcted] // grabs value from nearbyPlacesObj via index
-      // })
-      // let finalNameVal = ""
-      // nearbyPlacesObjs.map((placeName) => {
-      //   if(placeName.title + " " + placeName.address === item){
-      //     finalNameVal = placeName.title + " " + placeName.address
-      //   }
-      // })
-      // console.log(finalNameVal)
-
-      // let removeMarkerFromObj = nearbyPlacesObjs[objLocation]
-      // let removeMarker = markers.filter(marker => marker.title + " " + removeMarkerFromObj.address != item)
-      let removeMarker = markers.filter(marker => item != item)
-      console.log(removeMarker)
-      // markers.map((marker) => {
-      //   if(marker.title + " " + marker.address == item) {
-      //     let removeVal = marker.title + " " + marker.address
-      //     lat arrVal = markers.filter(markerz => markerz.title + " " + .address != removeVal)
-      //   }
-      // })
-
-      // console.log(removeMarker)
-      // setMarkers(removeMarker)
+      let removeMarker = markers.filter(marker => marker.title + " " + marker.address != item)
+      setMarkers(removeMarker)
       let arrVal = selectedPlaces.filter(places => places != item);// get marker value to be removed and added to markers without duplicates
       setSelectedPlaces(arrVal);
     }
@@ -461,7 +436,6 @@ const Search = () => {
         /> */}
       </MapView>
 
-
       <View className= {mapOpened ? "h-3/4 bg-LineBuddyBlue rounded-t-lg bottom-0 inset-x-0 absolute": "h-16 bg-LineBuddyBlue rounded-t-lg bottom-0 inset-x-0 absolute"}>
         <TouchableOpacity onPress={() => {setMapOpened(!mapOpened)}}  className = "items-end p-2">
           <Triangle
@@ -502,24 +476,28 @@ const Search = () => {
                 //   </TouchableOpacity>
                 // )}
                   />
-        </View>
-        }
-      </View>
-
-
+                  </View>
+                  }
+                </View>
           
+          
+                    
+          
+          
+                
+          
+          
+                {/* <View className= "h-20 bg-red-400 absolute top-20">
+                      <FlatList
+                      data = {filteredPlaces}
+                      renderItem={({item}) => <Text style={styles.lists3}>{item}</Text>}
+                      
+                      />
+                </View> */}
+                
 
 
-      
 
-
-      {/* <View className= "h-20 bg-red-400 absolute top-20">
-            <FlatList
-            data = {filteredPlaces}
-            renderItem={({item}) => <Text style={styles.lists3}>{item}</Text>}
-            
-            />
-      </View> */}
       
     </View>
   );
@@ -575,6 +553,47 @@ const styles = StyleSheet.create({
 
 })
 export default Search
+
+
+
+
+
+
+// dis shit 
+
+// {mapOpened && 
+//   <View className = "h-5/6">
+//     <Autocomplete
+//         className="top-0"
+//         data={filteredPlaces}
+//         flatListProps={{
+//           keyExtractor: (_, idx) => idx,
+//           renderItem: ({ item }) => <View className="h-20 p-2 bg-LineBuddyBlue flex border-solid border-LineBuddyBlue border-8"><Text className = "bg-white p-2 w-5/6 border-solid border-black border text-center font-bold drop-shadow-xl">{item}</Text><CheckBox
+//           onValueChange={() => {selectedPlacesObj(item)}}// figure out how to get setselected value working
+//           className = "bg-white self-end absolute top-5 right-5 p-3 border-solid border-black"// and y u cant pass value in through fucntio to change state
+//           value={selectedPlaces.includes(item)}
+//         /></View> 
+//         }}
+//         // inputContainerStyle={styles.lists3}
+//         // listContainerStyle = {styles.lists2}
+        
+//         onChangeText={(text) => findPlace(text)}
+//         placeholder="Enter the film title"
+//         // renderItem={({item}) => (
+//         //   <TouchableOpacity // fix up the the code and make it look pretty
+//         //     // onPress={() =>  {
+//         //     //   setSelectedValue(item);
+//         //     //   setFilteredPlaces([]);
+//         //     // }}>
+//         //     onPress={() => console.log("Kjdjks")}
+//         //     >
+//         //     {/* <Text className ="h-20">{item}</Text> */}
+//         //     {/* <Text className="h-10 p-3 w- bg-red-400">Hello</Text> */}
+//         //   </TouchableOpacity>
+//         // )}
+//           />
+// </View>
+// }
 
 
 {/* <Autocomplete
